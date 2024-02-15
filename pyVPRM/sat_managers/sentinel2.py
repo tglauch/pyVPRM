10be3aa13525f8cee8d7c4f07aebd043510bb5e9
Theo Glauch <theo.glauch@tum.de>
2024-02-15 09:06:22 +0100

    Update sentinel2.py

--- pyVPRM/sat_managers/sentinel2.py
+++ pyVPRM/sat_managers/sentinel2.py
@@ -138,14 +138,14 @@ class sentinel2(satellite_data_manager):
         if bands is None:
             bands=self.bands
         self.sat_img[bands] = xr.where((self.sat_img['scl']==9) | (self.sat_img['scl']==8) | (self.sat_img['scl']==10),
-                                       -np.inf, self.sat_img[bands])
+                                       np.nan, self.sat_img[bands])
         return
       
     def mask_snow(self, bands=None):
         if bands is None:
             bands=self.bands
         self.sat_img[bands] = xr.where((self.sat_img['scl']==11),
-                                       np.nan, self.sat_img[bands])
+                                       -np.inf, self.sat_img[bands])
         return
     
     def get_recording_time(self):
