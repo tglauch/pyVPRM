5a2dd46eea0a293e1df7b50887e76b4315e4cac3
Theo Glauch <theo.glauch@tum.de>
2024-10-23 13:03:01 +0200

    Update viirs09ga.py

--- pyVPRM/sat_managers/viirs09ga.py
+++ pyVPRM/sat_managers/viirs09ga.py
@@ -44,9 +44,6 @@ class VIIRS(earthdata):
         self.product = 'VNP09GA.001' 
         self.pixel_size = 463.312716527778
 
-    def get_files(self, dest):
-        return glob.glob(os.path.join(dest, "*.h5"))
-
     def set_sat_img(self, ind):
         # implements ones M and L bands are used. Currently only M bands implemented.
         return
@@ -76,32 +73,52 @@ class VIIRS(earthdata):
         return
 
     def mask_bad_pixels(self, bands=None):
+        # Bands are hardcoded in this implementation. That is fine for standard VPRM. 
+        # Revise if necessary.
+        
         if bands is None:
             bands = self.bands
 
-        band_nums = [int(band[-1]) for band in bands]
-        masks = dict()
-
-        for b in band_nums:
-            if b > 2:  ## VIIRS only has quality mask for band1 and band2
-                continue
-            start_bit = b * 4  # Start Bit
-            end_bit = b * 4 + 3  # End Bit  (inclusive)
-            num_bits_to_extract = end_bit - start_bit + 1
-            bit_mask = (1 << num_bits_to_extract) - 1
-            masks[b] = (
-                np.array(self.sat_img["SurfReflect_QC_500m"].values, dtype=np.uint32)
+        start_bit = 3  # Start Bit
+        end_bit = 3  # End Bit  (inclusive)
+        num_bits_to_extract = end_bit - start_bit + 1
+        bit_mask = (1 << num_bits_to_extract) - 1
+        mask1 = (
+            (
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF6_1'].values, dtype=np.uint32)
                 >> start_bit
-            ) & bit_mask
-
-        for mask_int in masks.keys():
-            masks[mask_int] = masks[mask_int] != int("0000", 2)
-            self.sat_img["SurfReflect_I{}".format(mask_int)] = xr.where(
-                masks[mask_int],
-                np.nan,
-                self.sat_img["SurfReflect_I{}".format(mask_int)],
             )
-            # self.sat_img['SurfReflect_I{}'.format(mask_int)].values[masks[mask_int]] = np.nan
+            & bit_mask
+        ) != int("0", 2)
+        
+        start_bit = 4  # Start Bit
+        end_bit = 4  # End Bit  (inclusive)
+        num_bits_to_extract = end_bit - start_bit + 1
+        bit_mask = (1 << num_bits_to_extract) - 1
+        mask2 = (
+            (
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF6_1'].values, dtype=np.uint32)
+                >> start_bit
+            )
+            & bit_mask
+        ) != int("0", 2)
+        
+        start_bit = 5  # Start Bit
+        end_bit = 5  # End Bit  (inclusive)
+        num_bits_to_extract = end_bit - start_bit + 1
+        bit_mask = (1 << num_bits_to_extract) - 1
+        mask3 = (
+            (
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF6_1'].values, dtype=np.uint32)
+                >> start_bit
+            )
+            & bit_mask
+        ) != int("0", 2)
+        
+        mask= mask1 | mask2 | mask3
+
+        for b in bands:
+            self.sat_img[b] = xr.where(mask, np.nan, self.sat_img[b])
         return
 
     def mask_clouds(self, bands=None):
@@ -114,7 +131,7 @@ class VIIRS(earthdata):
         bit_mask = (1 << num_bits_to_extract) - 1
         mask1 = (
             (
-                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF1_1'].values, dtype=np.uint32)
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF1_1'].values, dtype=np.uint32)
                 >> start_bit
             )
             & bit_mask
@@ -127,7 +144,7 @@ class VIIRS(earthdata):
         bit_mask = (1 << num_bits_to_extract) - 1
         mask2 = (
             (
-                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
                 >> start_bit
             )
             & bit_mask
@@ -146,7 +163,7 @@ class VIIRS(earthdata):
         bit_mask = (1 << num_bits_to_extract) - 1
         mask = (
             (
-                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
+                np.array(self.sat_img['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
                 >> start_bit
             )
             & bit_mask
@@ -154,20 +171,3 @@ class VIIRS(earthdata):
         for b in bands:
             self.sat_img[b] = xr.where(mask, np.inf, self.sat_img[b])
         return
-
-    def get_recording_time(self):
-        date0 = datetime.strptime(
-            self.meta_data["RangeBeginningDate"]
-            + "T"
-            + self.meta_data["RangeBeginningTime"]
-            + "Z",
-            "%Y-%m-%dT%H:%M:%S.%fZ",
-        )
-        date1 = datetime.strptime(
-            self.meta_data["RangeEndingDate"]
-            + "T"
-            + self.meta_data["RangeEndingTime"]
-            + "Z",
-            "%Y-%m-%dT%H:%M:%S.%fZ",
-        )
-        return date0 + (date1 - date0) / 2
