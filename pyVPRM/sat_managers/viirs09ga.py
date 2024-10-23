b5fcd085ceba99c1baa0ef15750702f90cb03ba9
Theo Glauch <theo.glauch@tum.de>
2024-10-23 12:52:58 +0200

    Update viirs09ga.py

--- pyVPRM/sat_managers/viirs09ga.py
+++ pyVPRM/sat_managers/viirs09ga.py
@@ -42,7 +42,7 @@ class VIIRS(earthdata):
         self.sat = "VIIRS"
         self.path = "VIIRS"
         self.product = 'VNP09GA.001' 
-        self.pixel_size = 463.31271652777775
+        self.pixel_size = 463.312716527778
 
     def get_files(self, dest):
         return glob.glob(os.path.join(dest, "*.h5"))
@@ -61,63 +61,18 @@ class VIIRS(earthdata):
         self.bands = bands
 
     def individual_loading(self):
-        self.sat_img = rxr.open_rasterio(self.sat_image_path, masked=True, cache=False)
-        f = h5py.File(self.sat_image_path, "r")
-
-        fileMetadata = f["HDFEOS INFORMATION"]["StructMetadata.0"][()].split()
-        fileMetadata = [m.decode("utf-8") for m in fileMetadata]
-        ulc = [i for i in fileMetadata if "UpperLeftPointMtrs" in i][0]
-        west = float(ulc.split("=(")[-1].replace(")", "").split(",")[0])
-        north = float(ulc.split("=(")[-1].replace(")", "").split(",")[1])
-        del f
-
-        if isinstance(self.sat_img, list):
-            self.sat_img = self.sat_img[0].squeeze()
-            self.keys = [i for i in list(self.sat_img.data_vars) if "_M" in i]
-            self.sat_img = self.sat_img[self.keys]
-        else:
-            self.sat_img = self.sat_img.squeeze()
-            self.keys = self.sat_img.data_vars
-
-        crs_str = 'PROJCS["unnamed",\
-                    GEOGCS["Unknown datum based upon the custom spheroid", \
-                    DATUM["Not specified (based on custom spheroid)", \
-                    SPHEROID["Custom spheroid",6371007.181,0]], \
-                    PRIMEM["Greenwich",0],\
-                    UNIT["degree",0.0174532925199433]],\
-                    PROJECTION["Sinusoidal"], \
-                    PARAMETER["longitude_of_center",0], \
-                    PARAMETER["false_easting",0], \
-                    PARAMETER["false_northing",0], \
-                    UNIT["Meter",1]]'
-
-        rename_dict = dict()
-        for i in self.keys:
-            rename_dict[i] = i.split("Data_Fields_")[1]
-        self.sat_img = self.sat_img.rename(rename_dict)
-        self.keys = np.array(list(self.sat_img.data_vars))
-        bands = []
-        for k in self.keys:
-            if ("SurfReflect_I" not in k) & ("SurfReflect_M" not in k):
-                continue
-            sf = self.sat_img.attrs[
-                [
-                    i
-                    for i in self.sat_img.attrs
-                    if ("scale_factor" in i) & ("err" not in i) & (k in i)
-                ][0]
-            ]
-            self.sat_img[k] = self.sat_img[k] * sf
-            bands.append(k)
-        self.bands = bands
-        transform = Affine(self.pixel_size, 0, west, 0, -self.pixel_size, north)
-        coords = affine_to_coords(
-            transform, self.sat_img.rio.width, self.sat_img.rio.height
-        )
-        self.sat_img.coords["x"] = coords["x"]
-        self.sat_img.coords["y"] = coords["y"]
-        self.sat_img.rio.write_crs(crs_str, inplace=True)
-        self.meta_data = self.sat_img.attrs
+        t = rxr.open_rasterio(self.sat_image_path, masked=True, cache=False)
+        obs_time = parser.parse(t[0].attrs['RangeBeginningDate']) + timedelta(hours=12)
+        for key in ['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF1_1',
+                    'HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1',
+                    'HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF6_1']:
+            test = t[0][key].sel(x=t[1].coords['x'], y=t[1].coords['y'], method='nearest')
+            test.coords['x'] = t[1].coords['x']
+            test.coords['y'] = t[1].coords['y']
+            t[1][key] = test 
+        t[1]= t[1].assign_coords({'time': obs_time})
+        self.sat_img = t[1]
+        self.set_band_names()
         return
 
     def mask_bad_pixels(self, bands=None):
@@ -153,66 +108,52 @@ class VIIRS(earthdata):
         if bands is None:
             bands = self.bands
 
-        start_bit = 0  # Start Bit
-        end_bit = 1  # End Bit  (inclusive)
+        start_bit = 2  # Start Bit
+        end_bit = 3  # End Bit  (inclusive)
         num_bits_to_extract = end_bit - start_bit + 1
         bit_mask = (1 << num_bits_to_extract) - 1
         mask1 = (
             (
-                np.array(self.sat_img["SurfReflect_State_500m"].values, dtype=np.uint32)
+                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF1_1'].values, dtype=np.uint32)
                 >> start_bit
             )
             & bit_mask
         ) != int("00", 2)
-
-        start_bit = 2  # Start Bit
-        end_bit = 2  # End Bit  (inclusive)
+        
+        #Cloud Shadow
+        start_bit = 3  # Start Bit
+        end_bit = 3  # End Bit  (inclusive)
         num_bits_to_extract = end_bit - start_bit + 1
         bit_mask = (1 << num_bits_to_extract) - 1
         mask2 = (
             (
-                np.array(self.sat_img["SurfReflect_State_500m"].values, dtype=np.uint32)
+                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
                 >> start_bit
             )
             & bit_mask
         ) != int("0", 2)
 
-        start_bit = 8  # Start Bit
-        end_bit = 9  # End Bit  (inclusive)
-        num_bits_to_extract = end_bit - start_bit + 1
-        bit_mask = (1 << num_bits_to_extract) - 1
-        mask3 = (
-            (
-                np.array(self.sat_img["SurfReflect_State_500m"].values, dtype=np.uint32)
-                >> start_bit
-            )
-            & bit_mask
-        ) != int("00", 2)
-
         for b in bands:
-            self.sat_img[b] = xr.where((mask1 | mask2 | mask3), np.nan, self.sat_img[b])
-            # self.sat_img[b].values[mask != int('00', 2)] = np.nan
+            self.sat_img[b] = xr.where((mask1 | mask2), np.nan, self.sat_img[b])
         return
 
     def mask_snow(self, bands=None):
         if bands is None:
             bands = self.bands
-        start_bit = 12  # Start Bit
-        end_bit = 12  # End Bit  (inclusive)
+        start_bit = 5  # Start Bit
+        end_bit = 5  # End Bit  (inclusive)
         num_bits_to_extract = end_bit - start_bit + 1
         bit_mask = (1 << num_bits_to_extract) - 1
         mask = (
-            np.array(self.sat_img["SurfReflect_State_500m"].values, dtype=np.uint32)
-            >> start_bit
-        ) & bit_mask
+            (
+                np.array(t[1]['HDFEOS_GRIDS_VNP_Grid_1km_2D_Data_Fields_SurfReflect_QF2_1'].values, dtype=np.uint32)
+                >> start_bit
+            )
+            & bit_mask
+        ) != int("0", 2)
         for b in bands:
-            self.sat_img[b] = xr.where(mask == int("1", 2), np.inf, self.sat_img[b])
-            # self.sat_img[b].values[mask == int('1', 2)] = np.inf
+            self.sat_img[b] = xr.where(mask, np.inf, self.sat_img[b])
         return
-
-    def get_cloud_coverage(self):
-        logger.info("PercentCloudy", self.meta_data["PercentCloudy"])
-        return self.meta_data["PercentCloudy"]
 
     def get_recording_time(self):
         date0 = datetime.strptime(
