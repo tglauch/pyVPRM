48e1ad27e9ebca9000b5a5bbea9236412f91291f
Theo Glauch <theo.glauch@tum.de>
2026-03-12 14:13:45 +0100

    Refactor get_training_data and add animation method

--- pyVPRM/vprm_models/vprm_nn_model.py
+++ pyVPRM/vprm_models/vprm_nn_model.py
@@ -52,25 +52,25 @@ class pyvprnn:
         self.ffp_handler = ffp_handler
         return
 
-    def get_training_data(self, opath):
-        ds = vprm_pre.sat_imgs.sat_img.drop(['scl', 'ndvi']).drop(['time'])
-        ds = ds.assign_attrs(crs=ds.rio.crs)
-        ds['min_evi'] = vprm_pre.min_max_evi.sat_img['min_evi']
-        ds['max_evi'] = vprm_pre.min_max_evi.sat_img['max_evi']
-        ds['th'] = vprm_pre.min_max_evi.sat_img['th']
-        ds['min_lswi'] =  vprm_pre.min_lswi.sat_img['min_lswi']
-        ds['max_lswi'] =  vprm_pre.max_lswi.sat_img['max_lswi']
+    def get_training_data(self, opath=None):
+        self.ds = self.vprm_pre.sat_imgs.sat_img.drop(['scl', 'ndvi']).drop(['time'])
+        self.ds = ds.assign_attrs(crs=ds.rio.crs)
+        self.ds['min_evi'] =self.vprm_pre.min_max_evi.sat_img['min_evi']
+        self.ds['max_evi'] = self.vprm_pre.min_max_evi.sat_img['max_evi']
+        self.ds['th'] = self.vprm_pre.min_max_evi.sat_img['th']
+        self.ds['min_lswi'] =  self.vprm_pre.min_lswi.sat_img['min_lswi']
+        self.ds['max_lswi'] =  self.vprm_pre.max_lswi.sat_img['max_lswi']
         flux_tower_keys = ['t2m', 'ssrd', 'ZL', 'FETCH_90',
                           'NEE_VUT_REF', 'GPP_DT_VUT_REF', 'RECO_DT_VUT_REF',
                            'NEE_VUT_REF_QC', 'GPP_NT_VUT_REF',
                            'RECO_NT_VUT_REF']
         for key in flux_tower_keys:
             try:
-                ds[key] = xr.DataArray(
-                    flux_tower_inst.flux_data[key].values,
+                self.ds[key] = xr.DataArray(
+                    self.flux_tower.flux_data[key].values,
                     dims=('datetime_utc',),
                     coords={
-                        'datetime_utc': flux_tower_inst.flux_data['datetime_utc']
+                        'datetime_utc': self.flux_tower.flux_data['datetime_utc']
                     },
                     attrs={'units': 'K', 'long_name': '2m air temperature'}
                 )
@@ -78,11 +78,11 @@ class pyvprnn:
                 print('Problem with {}'.format(key))
                 continue
         
-        t0 = np.datetime64(vprm_pre.timestamp_start)
+        t0 = np.datetime64(self.vprm_pre.timestamp_start)
         ds = ds.assign_coords(
             days_since_t0=(
                 "datetime_utc",
-                ((ds.datetime_utc.data - t0) / np.timedelta64(1, "D")).astype(int)))
+                ((self.ds.datetime_utc.data - t0) / np.timedelta64(1, "D")).astype(int)))
         
         if 'GPP_DT_VUT_REF' in list(ds.keys()):
             gpp_key = 'GPP_DT_VUT_REF'
@@ -92,19 +92,19 @@ class pyvprnn:
             print('No GPP variable available')
         
         mask = (
-            (ds["NEE_VUT_REF_QC"] < 2) &
-            (ds["ZL"] > -1000))
+            (self.ds["NEE_VUT_REF_QC"] < 2) &
+            (self.ds["ZL"] > -1000))
         
         footprint_timestamps = (
-            ds["datetime_utc"]
+            self.ds["datetime_utc"]
             .where(mask, drop=True))
         
         self.ffp_handler.make_calculation_grid()
         self.ffp_handler.calculate_footprints()
-        self.ffp_handler.regrid_calculation_grid_to_satellite_grid(handler.sat_img, base_path)
+        self.ffp_handler.regrid_calculation_grid_to_satellite_grid(self.vprm_pre.sat_imgs.sat_img, base_path)
         
-        ds['ffp_footprint'] = self.ffp_handler.footprint_on_satellite_grid['footprint']
-        ds['land_cover_map'] = vprm_pre.land_cover_type.sat_img
+        self.ds['ffp_footprint'] = self.ffp_handler.footprint_on_satellite_grid['footprint']
+        self.ds['land_cover_map'] = self.vprm_pre.land_cover_type.sat_img
         
         meteo_vars = {'ssrd': lambda x: x/3600,
                       't2m': lambda x: x - 273.15,
@@ -116,8 +116,8 @@ class pyvprnn:
                       'stl1': lambda x: x - 273.15,
                       'stl2': lambda x: x - 273.15,}
         
-        self.era5_inst.reduce_time(flux_tower_inst.flux_data['datetime_utc'].iloc[0],
-                         flux_tower_inst.flux_data['datetime_utc'].iloc[-1])
+        self.era5_inst.reduce_time(self.flux_tower.flux_data['datetime_utc'].iloc[0],
+                         self.flux_tower.flux_data['datetime_utc'].iloc[-1])
         
         self.era5_inst.ds_out = sel_nearest_valid(self.era5_inst.ds_out, lon, lat) 
         for k in list(meteo_vars.keys()):
@@ -131,101 +131,79 @@ class pyvprnn:
         self.era5_inst.ds_out['vpd_decorrelated_log'] = np.log(np.maximum(self.era5_inst.ds_out['vpd'], 0.01)) - np.log(es)
         for key in self.era5_inst.ds_out.keys():
             print(key)
-            ds[key+'_era5'] = self.era5_inst.ds_out[key].sel({'valid_time': ds['datetime_utc']}, method='nearest')
+            self.ds[key+'_era5'] = self.era5_inst.ds_out[key].sel({'valid_time': self.ds['datetime_utc']}, method='nearest')
         
         all_meteo_vars = np.concatenate([['t2m', 'ssrd', 'NEE_VUT_REF', gpp_key, 'RECO_DT_VUT_REF'],
                                         [i+'_era5' for i in self.era5_inst.ds_out.keys()]])
-        meteos = ds[all_meteo_vars].sel({'datetime_utc': ds['t']})
+        meteos = self.ds[all_meteo_vars].sel({'datetime_utc': self.ds['t']})
         
         sat_vars = ['lswi','evi', 'nirv', 'ndre']
-        ds[sat_vars] = (
-            ds[sat_vars]
+        self.ds[sat_vars] = (
+            self.ds[sat_vars]
             .fillna(0.0)
         )
-        ds["ffp_footprint"] = ds["ffp_footprint"].fillna(0.0)
-        sat_imgs = ds[sat_vars].sel({'time_gap_filled': meteos['days_since_t0']})
-
-
-    def make_sat_imgs_plot(self, ind_x, ind_y, opath=None):
-        from pyVPRM.lib.fancy_plot import *
-        import matplotlib.colors as mcolors
-        import matplotlib.gridspec as gridspec
+        self.ds["ffp_footprint"] = self.ds["ffp_footprint"].fillna(0.0)
+        if opath is not None:
+            self.ds.to_netcdf(opath)
+    
+    def make_satellite_animation(self, opath=None)
+        from pyproj import Transformer
+        lon, lat = self.flux_tower.get_lonlat()
+        transformer = Transformer.from_crs(
+            "EPSG:4326",
+            ds.rio.crs,   # or "EPSG:32632"
+            always_xy=True)
+        x_utm, y_utm = transformer.transform(lon, lat)
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from matplotlib.animation import FuncAnimation, PillowWriter
         
-        timestamp_start_np = np.datetime64(vprm_pre.timestamp_start)  # convert to numpy.datetime64
-        days_array = vprm_pre.sat_imgs.sat_img['time_gap_filled'].values  # NumPy array
-        
+        # -------------------------
+        # Precompute limits once
+        # -------------------------
+        vmax = np.percentile(self.ds['evi'], 99.5)
+        vmin = np.percentile(self.ds['evi'], 0.5)
+        timestamp_start_np = np.datetime64(self.vprm_pre.timestamp_start)
+        days_array = self.vprm_pre.sat_imgs.sat_img['time_gap_filled'].values
         plt_times = timestamp_start_np + np.timedelta64(1, 'D') * days_array
         
-        plt_times_pre_smoothing = timestamp_start_np + np.timedelta64(1, 'D') * handler_pre_smoothing['time']
-        # --- Extract values ---
-        nirv_pre  = handler_pre_smoothing['nirv'].values[:, ind_x, ind_y]
-        evi_pre   = handler_pre_smoothing['evi'].values[:, ind_x, ind_y]
-        ndre_pre  = handler_pre_smoothing['ndre'].values[:, ind_x, ind_y]
-        evi2_pre  = handler_pre_smoothing['evi2'].values[:, ind_x, ind_y]
-        lswi_pre  = handler_pre_smoothing['lswi'].values[:, ind_x, ind_y]
-        scl_vals  = handler_pre_smoothing['scl'].values[:, ind_x, ind_y]
+        ntime = self.ds.dims["time_gap_filled"]
+        fig, ax = newfig(0.9, 0.7)
         
-        nirv_smooth = vprm_pre.sat_imgs.sat_img['nirv'].values[:, ind_x, ind_y]
-        evi_smooth  = vprm_pre.sat_imgs.sat_img['evi'].values[:, ind_x, ind_y]
-        ndre_smooth = vprm_pre.sat_imgs.sat_img['ndre'].values[:, ind_x, ind_y]
-        evi2_smooth = vprm_pre.sat_imgs.sat_img['evi2'].values[:, ind_x, ind_y]
-        lswi_smooth = vprm_pre.sat_imgs.sat_img['lswi'].values[:, ind_x, ind_y]
+        xmin, ymin, xmax, ymax = self.ds.rio.bounds()
+        im = ax.imshow(
+            self.ds['evi'].isel(time_gap_filled=0).values,
+            cmap="Greens",
+            vmin=vmin,
+            vmax=vmax,
+            origin="upper",
+            extent=[xmin, xmax, ymin, ymax],
+            animated=True
+        )
         
-        # --- Discrete SCL colormap (0–11) ---
-        cmap = plt.cm.get_cmap('tab20', 12)
-        norm = mcolors.BoundaryNorm(np.arange(-0.5, 12.5, 1), cmap.N)
+        tower = ax.scatter(x_utm, y_utm, marker="*", color="k", zorder=3)
+        title = ax.set_title(str(plt_times[0])[:10])
+        ax.axis("off")
+        cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+        cbar.set_label("EVI")
+        def update(frame):
+            im.set_array(ds['evi'].isel(time_gap_filled=frame).values)
+            title.set_text(str(plt_times[frame])[:10])
+            return im, title
+        ani = FuncAnimation(
+            fig,
+            update,
+            frames=ntime,
+            interval=150,     # ms between frames
+            blit=True)
         
-        # --- Figure with 3 panels ---
-        fig = plt.figure(figsize=figsize(1.0, 1.0))
-        gs = gridspec.GridSpec(5, 2, width_ratios=[30, 1], wspace=0.05)
-        
-        axes = [fig.add_subplot(gs[i, 0]) for i in range(5)]
-        cax = fig.add_subplot(gs[:, 1])  # colorbar spans all rows
-        
-        indices = [
-            ("NIRv", nirv_pre, nirv_smooth),
-            ("EVI",  evi_pre,  evi_smooth),
-            ("NDRE", ndre_pre, ndre_smooth),
-            ("EVI2", evi2_pre, evi2_smooth),
-            ("LSWI", lswi_pre, lswi_smooth),
-        ]
-        
-        for i, (ax, (name, raw_vals, smooth_vals)) in enumerate(zip(axes, indices)):
-        
-            sc = ax.scatter(
-                plt_times_pre_smoothing,
-                raw_vals,
-                c=scl_vals,
-                cmap=cmap,
-                norm=norm,
-                s=5,
-                alpha=0.9
-            )
-        
-            ax.plot(
-                plt_times,
-                smooth_vals,
-                color='k',
-                lw=1.5
-            )
-        
-            ax.set_ylabel(name)
-            # ax.set_ylim(-0.1, 1)
-            ax.grid(alpha=0.3)
-            # Only show x-axis tick labels for bottom panel
-            if i < 3:
-                ax.set_xticklabels([])
-        
-        axes[-1].set_xlabel("Time")
-        
-        # --- Shared colorbar ---
-        cbar = fig.colorbar(sc, cax=cax, ticks=np.arange(12))
-        cbar.set_label("SCL class")
-
         if opath is not None:
-            fig.savefig(opath=None,dpi=300, bbox_inches='tight')
-
+            ani.save(
+                opath,
+                writer=PillowWriter(fps=6)
+            )
     
-
-  
+        
+    
       
+          
